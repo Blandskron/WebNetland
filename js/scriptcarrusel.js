b2a73226e2e3e841ad5b298ec426173942ecf092
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-10-29 17:22:44 -0300

    netlandweb

---
+++ js/scriptcarrusel.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    function showSlide(n) {
+        slides[currentSlide].style.display = "none";
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].style.display = "block";
+    }
+
+    showSlide(currentSlide);
+});
